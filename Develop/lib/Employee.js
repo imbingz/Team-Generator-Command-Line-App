9ec6a12236b4ce720c17dc400ca09bc8d7ef7051
Bing Z <bing@bings-mbp.lan>
2020-09-26 22:47:19 -0600

    passed 2nd test

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,8 +1,8 @@
 // TODO: Write code to define and export the Employee class
 
 class Employee {
-  constructor() {
-    console.log('im constructor');
+  constructor(name) {
+    this.name = name
   }
 }
 
