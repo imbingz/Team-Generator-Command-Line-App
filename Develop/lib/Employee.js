852e2831c0b7d3b99a95be99a6a09c6efe490933
Bing Z <bing@bings-mbp.lan>
2020-09-26 22:46:33 -0600

    passed first test

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,15 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee {
+  constructor() {
+    console.log('im constructor');
+  }
+}
+
+const e = new Employee
+
+
+
+
+//Export the Employee Class
+module.exports = Employee;
